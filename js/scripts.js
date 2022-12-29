dde145dacd7411f84f27163c141158a5c3f58cfe
MAbdurahman <mdbdrrhm@gmail.com>
2022-12-29 04:03:58 -0600

    Add preloader gif with its styles and script

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 /*=============================================
           js-simple-todo-list scripts
 ================================================*/
+/* =========================================
+            Preloader
+============================================ */
+$(window).on('load', function () {
+    // makes sure that whole site is loaded
+    $('#preloader-gif, #preloader').fadeOut(3000, function () {});
+});
+
 window.onload = function () {
 
     //**************** variables ****************//
